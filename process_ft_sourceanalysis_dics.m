cbd42b46f8c4c1188a68d75547dc4222cff89490
vahab <youssofzadeh-v@email.ulster.ac.uk>
2021-12-15 10:59:13 -0600

    Update process_ft_sourceanalysis_dics.m

--- process_ft_sourceanalysis_dics.m
+++ process_ft_sourceanalysis_dics.m
@@ -4,12 +4,12 @@ function varargout = process_ft_sourceanalysis_dics(varargin )
 % @=============================================================================
 % This function is part of the Brainstorm software:
 % https://neuroimage.usc.edu/brainstorm
-%
-% Copyright (c)2000-2019 University of Southern California & McGill University
+% 
+% Copyright (c)2000-2020 University of Southern California & McGill University
 % This software is distributed under the terms of the GNU General Public License
 % as published by the Free Software Foundation. Further details on the GPLv3
 % license can be found at http://www.gnu.org/copyleft/gpl.html.
-%
+% 
 % FOR RESEARCH PURPOSES ONLY. THE SOFTWARE IS PROVIDED "AS IS," AND THE
 % UNIVERSITY OF SOUTHERN CALIFORNIA AND ITS COLLABORATORS DO NOT MAKE ANY
 % WARRANTY, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO WARRANTIES OF
@@ -30,7 +30,7 @@ function sProcess = GetDescription() %#ok<DEFNU>
     sProcess.Comment     = 'FieldTrip: ft_sourceanalysis (DICS)';
     sProcess.Category    = 'Custom';
     sProcess.SubGroup    = 'Sources';
-    sProcess.Index       = 357; 
+    sProcess.Index       = 357;
     sProcess.Description = 'https://github.com/vyoussofzadeh/DICS-beamformer-for-Brainstorm';
     % Definition of the input accepted by this process
     sProcess.InputTypes  = {'data'};
